423051657d5ef4a5a3c132787c67091b40532cf7
Feeder1129 <markrevenmacaspac@gmail.com>
2023-10-31 22:05:23 +0800

    initial commit

--- Deities Unleashed/Assets/Scripts/Arrow.cs
+++ Deities Unleashed/Assets/Scripts/Arrow.cs
@@ -9,6 +9,10 @@ public class Arrow : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float impactForce = 40f;
 
+    // Define the tags that the arrow should compare against
+    private string[] targetTags = { "Phoenix", "Tiyanak", "BalBal", "TikTik", "Golem", "Wolf", "Cyclops", "ElectricGolem", "Eagle" };
+
+
     void Awake()
     {
         Destroy(gameObject, life);
@@ -16,18 +20,22 @@ public class Arrow : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
+        foreach (string targetTag in targetTags)
         {
-            EnemyTarget enemyDamageReceiver = collision.gameObject.GetComponent<EnemyTarget>();
-            if (enemyDamageReceiver != null)
+            if (collision.gameObject.CompareTag(targetTag))
             {
-                enemyDamageReceiver.TakeDamage(damage);
-                Debug.Log("Applied Damage: " + damage);
+                EnemyTarget enemyDamageReceiver = collision.gameObject.GetComponent<EnemyTarget>();
+                if (enemyDamageReceiver != null)
+                {
+                    enemyDamageReceiver.TakeDamage(damage);
+                    Debug.Log("Applied Damage: " + damage);
+                }
+                Destroy(gameObject);
+                return; // Exit the loop once we've found a matching tag
             }
         }
 
+        // If no matching tag is found, simply destroy the arrow
         Destroy(gameObject);
     }
-
-
 }
